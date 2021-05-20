2bb626c9ec968d278d2b9db569dd775ada929a36
Justin <justinrsilver@gmail.com>
2021-05-20 19:58:20 -0400

    Initial layout top nav and footer

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,60 @@
 <template>
   <v-app>
+
+    <!-- Navigation to different site pages -->
     <v-app-bar
       app
       color="dark"
       dark
     >
+    <span class="text-no-wrap text-h4">
+      <span class="">JUSTIN </span>
+      <span class="font-weight-bold">SILVER</span>
+    </span>
+
+      <!-- I want the tabs centered, but i'm not sure how to align
+      them with the title thats aligned start. Gotta look into documentation
+      for tabs and title in app bar -->
+      <v-tabs
+        right
+        class=""
+        color="light darken-1"
+      >
+        <v-tab
+          v-for="link in links"
+          :key="link"
+        >
+          {{ link }}
+        </v-tab>
+      </v-tabs>
+
     </v-app-bar>
 
+    <!-- Actual view that we're rendering with vue-router -->
     <v-main>
       <router-view/>
     </v-main>
+
+    <!-- Footer for my name, GitHub and LinkedIn links and butterCMS -->
+ <v-footer
+    light
+  >
+        <strong class="subheading mr-5">2021 Justin Silver</strong>
+
+        <v-spacer></v-spacer>
+
+        <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          class=""
+          icon
+        >
+          <v-icon size="24px">
+            {{ icon }}
+          </v-icon>
+        </v-btn>
+  </v-footer>
+
   </v-app>
 </template>
 
@@ -19,7 +64,15 @@ export default {
   name: 'App',
 
   data: () => ({
-    //
+    links: [
+      'Home',
+      'Projects',
+      'Contact',
+    ],
+    icons: [
+      'mdi-github',
+      'mdi-linkedin',
+    ]
   }),
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,29 @@
 <template>
-  <UnderConstruction/>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-img
+          alt="Electronics image"
+          contain
+          src="https://www.nutsvolts.com/uploads/articles/NV_0704_Christopherson_Large.jpg"
+          transition="scale-transition"
+          max-height="200"
+        />
+      </v-col>
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">
+          Welcome to Justin Silver's portfolio website
+        </h1>
+
+        <p class="subheading font-weight-regular display-1">
+          Insert description
+        </p>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-  import UnderConstruction from '../components/UnderConstruction'
-
   export default {
-    name: 'Home',
-
-    components: {
-      UnderConstruction,
-    },
   }
 </script>
