99a3eac38123247d94ed6b4f66bf52bb66fe7f2f
Justin <justinrsilver@gmail.com>
2021-05-26 16:35:18 -0400

    Add some contact logic, validation, and UI notification

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -8,50 +8,80 @@
     <ColumnWrapper>
       <v-container fluid>
         <form>
+
           <v-text-field
             v-model="name"
             :error-messages="nameErrors"
-            :counter="10"
+            :counter="MAX_NAME_CHAR"
             label="Your name"
             required
             @input="$v.name.$touch()"
             @blur="$v.name.$touch()"
           ></v-text-field>
+
           <v-text-field
             v-model="email"
             :error-messages="emailErrors"
             label="Your e-mail"
             required
-            @input="$v.email.$touch()"
+            @change="$v.email.$touch()"
             @blur="$v.email.$touch()"
           ></v-text-field>
 
           <v-textarea
+            v-model="message"
+            :error-messages="messageErrors"
+            :counter="MAX_MSG_CHAR"
+            label="Your message"
+            required
+            @input="$v.message.$touch()"
+            @blur="$v.message.$touch()"
             clearable
             clear-icon="mdi-close-circle"
-            counter
-            label="Your message"
-            :rules="rules"
-            v-model="message"
           ></v-textarea>
 
-          <v-btn class="mr-4" @click="submit">
+          <v-btn :disabled='submitStatus === "PENDING"' @click="submit">
             submit
           </v-btn>
+              <v-progress-circular
+              class="ml-3"
+              v-if="submitStatus === 'PENDING'"
+      indeterminate
+      color="grey-darken-3"
+    ></v-progress-circular>
+
         </form>
       </v-container>
     </ColumnWrapper>
+      <!-- Visual indication to user for form submission -->
+      <!-- A bit of a hack to remove the black (dark) border around the content -->
+        <v-snackbar
+          v-model="snackbar"
+          :timeout="2500"
+          color="transparent"
+          elevation='0'
+        >
+        <v-alert type="success" v-if="submitStatus === 'OK'">Thanks for your submission!</v-alert>
+        <v-alert type="error" v-if="submitStatus === 'ERROR'">Please fill the form correctly.</v-alert>
+          </v-snackbar>
+
   </v-container>
+
   </div>
 </template>
 
 <script>
+
+const MAX_NAME_CHAR = 30;
+const MAX_MSG_CHAR = 1000;
+const MIN_MSG_CHAR = 10;
+
 import PageHeader from "../components/PageHeader.vue";
 import ColumnWrapper from "@/components/ColumnWrapper.vue";
 
 // Form validation libraries
 import { validationMixin } from "vuelidate";
-import { required, maxLength, email } from "vuelidate/lib/validators";
+import { required, minLength, maxLength, email } from "vuelidate/lib/validators";
 
 export default {
   components: {
@@ -59,17 +89,25 @@ export default {
     ColumnWrapper,
   },
   data: () => ({
-    rules: [(v) => v.length <= 25 || "Max 1000 characters"],
     message: "",
     name: "",
     email: "",
+
+    // Initialized in data section to avoid Vue errors (reference during render)
+    MAX_NAME_CHAR,
+    MAX_MSG_CHAR,
+    MIN_MSG_CHAR,
+
+    submitStatus: null,
+
+    snackbar: false,
   }),
 
   mixins: [validationMixin],
-
   validations: {
-    name: { required, maxLength: maxLength(10) },
+    name: { required, maxLength: maxLength(MAX_NAME_CHAR) },
     email: { required, email },
+    message: { required, minLength: minLength(MIN_MSG_CHAR), maxLength: maxLength(MAX_MSG_CHAR) },
   },
 
   computed: {
@@ -77,7 +115,7 @@ export default {
       const errors = [];
       if (!this.$v.name.$dirty) return errors;
       !this.$v.name.maxLength &&
-        errors.push("Name must be at most 10 characters long");
+        errors.push(`Name must be at most ${MAX_NAME_CHAR} characters long`);
       !this.$v.name.required && errors.push("Name is required.");
       return errors;
     },
@@ -88,11 +126,36 @@ export default {
       !this.$v.email.required && errors.push("E-mail is required");
       return errors;
     },
+    messageErrors() {
+      const errors = [];
+      if (!this.$v.message.$dirty) return errors;
+      !this.$v.message.minLength &&
+        errors.push(`Message must be at least ${MIN_MSG_CHAR} characters long`);
+      !this.$v.message.maxLength &&
+        errors.push(`Message must be at most ${MAX_MSG_CHAR} characters long`);
+      !this.$v.message.required && errors.push("Message is required.");
+      return errors;
+    },
   },
 
   methods: {
     submit() {
       this.$v.$touch();
+      if (this.$v.$invalid) {
+        this.submitStatus = 'ERROR'
+        this.snackbar = true;
+      } else {
+
+        // do your submit logic here
+        this.submitStatus = 'PENDING'
+
+        // timeout to display ok status
+        setTimeout(() => {
+          this.submitStatus = 'OK'
+          this.snackbar = true;
+        }, 500)
+
+      }
     },
   },
 };
